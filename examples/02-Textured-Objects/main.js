43f8afd4e35741370346af9d67fecffd75f5b78f
ubufu <ubufugu@gmail.com>
2016-11-06 16:22:07 -0800

    Scenenode: fixed scenegraph, added support for textures to scenenode objects. Renderer: fixed possible multi-parenting issue with scenenodes.

---
+++ examples/02-Textured-Objects/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ubufu on 9/7/2016.
+ */
+
+requirejs.config({
+  baseUrl: '',
+    paths: {
+      glmatrix: './third-party/gl-matrix-min',
+      scene: './mvre/SceneNode',
+      renderer: './mvre/Renderer',
+      engine: './mvre/Engine',
+      polyfill: './third-party/webvr-polyfill',
+      samples: './third-party/vr-samples-util',
+      basegame: 'baseGame',
+      cuon: './third-party/cuon-utils'
+    }
+});
+
+requirejs(['engine'], function(engine){
+
+
+});
---
+++ examples/02-Textured-Objects/mvre/Audio.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ubufu on 9/7/2016.
+ */
+
+/**
+ * global anonymous function. this is where all functions
+ * pertaining to objects in the class should be written.
+ */
+define(function () {
+    return {
+        load: function () {
+            console.log('audio loaded');
+        }
+    };
+});
